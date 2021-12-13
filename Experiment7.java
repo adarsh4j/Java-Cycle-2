ac2640e6af26f6bef08b814076e7f5da8a5dc87d
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2021-12-13 21:12:11 +0530

    Transpose of a given Matrix

---
+++ Experiment7.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {				
+
+		Scanner obj = new Scanner(System.in);
+
+		int a[][]=new int [10][10];
+
+		System.out.println("Enter the number of row: ");
+
+		int row = obj.nextInt();
+
+		System.out.println("Enter the number of column: ");
+
+		int col = obj.nextInt();
+
+		int i,j;
+
+		System.out.println("Enter the elements: ");
+
+		for(i=0;i<row;i++)
+
+		{		
+
+			for(j=0;j<col;j++)
+
+			{
+
+			a[i][j]=obj.nextInt();
+
+		    }
+
+		}
+
+		System.out.println("The tranpose matrix is");	
+
+		for(i=0;i<col;i++)
+
+		{
+
+			for(j=0;j<row;j++)
+
+			{
+
+			   System.out.print(a[j][i]+" ");	 
+
+		    }
+
+		    System.out.println();
+
+		}	
+
+	}
+
+}
